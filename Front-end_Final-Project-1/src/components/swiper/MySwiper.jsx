f9f0dcb5d94dd169fa94b0e7c64b768c4adf4d6a
Abed <abdulkareem.hajqasem@gmail.com>
2024-11-18 17:16:50 +0200

    nothing

--- Front-end_Final-Project-1/src/components/swiper/MySwiper.jsx
+++ Front-end_Final-Project-1/src/components/swiper/MySwiper.jsx
@@ -5,21 +5,15 @@ import HandpickedCard from '../handpickedcard/HandpickedCard';
 const MySwiper = () => {
   return (
     <Swiper
+      spaceBetween={50}
       slidesPerView={4}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
     >
-      <SwiperSlide>
-        <HandpickedCard></HandpickedCard>
-      </SwiperSlide>
-      <SwiperSlide>  <HandpickedCard></HandpickedCard></SwiperSlide>
-      <SwiperSlide>  <HandpickedCard></HandpickedCard></SwiperSlide>
-      <SwiperSlide>  <HandpickedCard></HandpickedCard></SwiperSlide>
-      <SwiperSlide>  <HandpickedCard></HandpickedCard></SwiperSlide>
-      <SwiperSlide>  <HandpickedCard></HandpickedCard></SwiperSlide>
-      <SwiperSlide>  <HandpickedCard></HandpickedCard></SwiperSlide>
-
-      ...
+      <SwiperSlide><HandpickedCard></HandpickedCard></SwiperSlide>
+      <SwiperSlide><HandpickedCard></HandpickedCard></SwiperSlide>
+      <SwiperSlide><HandpickedCard></HandpickedCard></SwiperSlide>
+      <SwiperSlide><HandpickedCard></HandpickedCard></SwiperSlide>
     </Swiper>
   );
 };
