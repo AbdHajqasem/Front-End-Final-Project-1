1323fde647b0c6384458bd06f8e22ab672b49ffb
Abdulkareem <abed.hajqasem@hotmail.com>
2024-11-18 17:13:49 +0200

    homepage-3

--- Front-end_Final-Project-1/src/App.jsx
+++ Front-end_Final-Project-1/src/App.jsx
@@ -1,12 +1,15 @@
 import NewArrivals from './components/newarrivals/NewArrivals'
 import MySwiper from './components/swiper/mySwiper'
+import './app.css'
 function App() {
 
 
   return (
     <>
+    <div className='main'>
 <NewArrivals></NewArrivals>
 <MySwiper></MySwiper>
+</div>
     </>
   )
 }
--- Front-end_Final-Project-1/src/components/swiper/MySwiper.jsx
+++ Front-end_Final-Project-1/src/components/swiper/MySwiper.jsx
@@ -1,11 +1,34 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useEffect,useState } from 'react';
 import 'swiper/css';
 import HandpickedCard from '../handpickedcard/HandpickedCard';
 
 const MySwiper = () => {
+  const [slidesPerView, setSlidesPerView] = useState(4);
+  useEffect(() => {
+    const updateSlidesPerView = () => {
+      const width = window.innerWidth;
+      if (width >= 1162) {
+        setSlidesPerView(4);
+      } else if (width >= 800) {
+        setSlidesPerView(3);
+      } else {
+        setSlidesPerView(2);
+      }
+    };
+
+    updateSlidesPerView();
+
+    window.addEventListener('resize', updateSlidesPerView);
+
+    return () => {
+      window.removeEventListener('resize', updateSlidesPerView);
+    };
+  }, []);
   return (
     <Swiper
-      slidesPerView={4}
+      spaceBetween={20}
+      slidesPerView={slidesPerView}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
     >
@@ -18,8 +41,6 @@ const MySwiper = () => {
       <SwiperSlide>  <HandpickedCard></HandpickedCard></SwiperSlide>
       <SwiperSlide>  <HandpickedCard></HandpickedCard></SwiperSlide>
       <SwiperSlide>  <HandpickedCard></HandpickedCard></SwiperSlide>
-
-      ...
     </Swiper>
   );
 };
