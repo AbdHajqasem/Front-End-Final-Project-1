32d3eb72c8c03ba6d2998d3cb9ac5cd0c3b07188
KhalidHani <khalidhani1996@gmail.com>
2024-11-24 13:09:45 +0300

    eidts on grid products

--- Front-end_Final-Project-1/src/App.jsx
+++ Front-end_Final-Project-1/src/App.jsx
@@ -2,12 +2,11 @@ import Footer from "./components/Footer";
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import ProductList from "./components/ProductList";
-import ProductList2 from "./components/ProductList2";
 
 const App = () => {
   return (
     <Router>
-      <ProductList2 />
+      <ProductList />
       <Footer />
     </Router>
   );
