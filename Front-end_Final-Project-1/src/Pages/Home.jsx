758b3c23d576d7f4363e95f373abdba9be8b73a0
MahmoudJD95 <mld95.doa@gmail.com>
2024-11-25 14:49:21 +0200

    create product page and routing

---
+++ Front-end_Final-Project-1/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import HeroSection from '../components/shearedComponent/HeroSection';
+import NavBar from '../components/shearedComponent/NavBar';
+
+export default function Home() {
+  return (
+    <>
+      <NavBar />
+      <HeroSection />
+    </>
+  );
+}
--- Front-end_Final-Project-1/src/main.jsx
+++ Front-end_Final-Project-1/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
+import Home from './Pages/Home.jsx';
+import Category from './Pages/Category.jsx';
+import Product from './Pages/Product.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const router = createBrowserRouter([{ path: '/', Component: App }]);
+const router = createBrowserRouter([
+  { path: '/', Component: Home },
+  { path: '/:category', Component: Category },
+  { path: '/:category/:product', Component: Product },
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
